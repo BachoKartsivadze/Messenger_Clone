b31586fb601a02f13770e59a22898f2760b069b6
Bacho <bkart21@freeuni.edu.ge>
2023-04-11 15:32:01 -0700

    Initial Commit

---
+++ Messenger_Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messenger_Clone
+//
+//  Created by bacho kartsivadze on 11.04.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
